629b5a2c6fa3d2662fda01a32ffa544339d833a0
Erik Lauer <elauer@paliotsolutions.com>
2025-07-09 15:38:53 -0400

    chore: add papaparse and dashboard shell

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen w-full bg-gray-50">
+      <Button>It works!</Button>
+    </main>
+  );
+}
